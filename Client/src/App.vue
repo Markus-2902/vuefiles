1928b67448a10764fedd9701bb698ab097cb84da
Markus-2902 <ullrich.m04@htlwienwest.at>
2024-10-22 23:47:51 +0200

    whatever

---
+++ Client/src/App.vue
@@ -0,0 +1,43 @@
+<script setup>
+
+</script>
+
+<template>
+  <header>
+    <q-tabs class="bg-black text-white">
+      <q-route-tab to="/" label="Welcome" class="text-white"></q-route-tab>
+      <q-route-tab
+        to="/goodbye"
+        label="Goodbye Page"
+        class="text-white"
+      ></q-route-tab>
+      <q-route-tab
+        to="/logout"
+        label="Abmeldescreen"
+        class="text-white"
+      ></q-route-tab>
+      <q-route-tab
+        to="/inputBasicData"
+        label="Eingabefelder"
+        class="text-white"
+      ></q-route-tab>
+    </q-tabs>
+  </header>
+
+  <RouterView />
+</template>
+
+<style>
+@font-face {
+  font-family: 'Montserrat';
+  src: url('/fonts/Montserrat/Montserrat-Regular.ttf') format('truetype');
+}
+
+* {
+  font-family: 'Montserrat';
+}
+
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+</style>
---
+++ Client/src/pages/goodbye.vue
@@ -0,0 +1,29 @@
+<script setup>
+</script>
+
+<template>
+  <div
+    style="
+      background-image: url('../../public/images/Background_Welcome.jpg'); /* Pfad zu deinem Bild */
+      background-position: left bottom; /* Positioniert das Bild unten links */
+      background-size: cover; /* Deckt den gesamten Container ab */
+      background-repeat: no-repeat; /* Verhindert das Wiederholen des Bildes */
+      min-height: 100vh; /* Optional: Setzt die Mindesthöhe auf die Bildschirmhöhe */
+    "
+    class="h-screen"
+  >
+    <div class="q-mx-xl column mt-10 items-center text-white pg">
+      <div class="col text-h3 pt-3">POI Guestbook</div>
+      <div class="col text-h6">Auf Wiedersehen!</div>
+      <div class="col text-h6">Wir hoffen, Ihnen hat es bei uns gefallen!</div>
+      <div class="">
+        <img src="../../public/images/raumplan1.png"/>
+      </div>
+      <div class="">
+        <img src="../../public/images/raumplan2.png"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style></style>
